167fc71b92f5312667f611fd52df6cf3b65661e8
JorgeMendesJumia <jorge.mendes@jumia.com>
2019-05-09 10:05:36 +0100

    fix setup and init

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
-setup(name='py_Heideltime',
+setup(name='py_heideltime',
       version='1.0',
       description='module in python using java standalone Heideltime from https://github.com/HeidelTime/heideltime',
       author='Jorge Alexandre Rocha Mendes',
       author_email='mendesjorge49@gmail.com',
       url='https://github.com/JMendes1995/py_heideltime.git',
-      packages=find_packages(include=['py_Heideltime']),
-      py_modules=['py_Heideltime']
+      packages=find_packages(include=['py_heideltime']),
+      py_modules=['py_heideltime']
 )
 
 
