300909b537d3e3b697ae50858bf00b58200ba161
JorgeMendesJumia <jorge.mendes@jumia.com>
2019-05-09 11:53:55 +0100

    add manifest

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
-files = ["py_heideltime/*"]
 setup(name='py_heideltime',
       version='1.0',
       description='module in python using java standalone Heideltime from https://github.com/HeidelTime/heideltime',
       author='Jorge Alexandre Rocha Mendes',
       author_email='mendesjorge49@gmail.com',
       url='https://github.com/JMendes1995/py_heideltime.git',
-      packages=find_packages(include=['py_heideltime', 'package']),
-      package_data = {'package' : files},
+      packages=find_packages(include=['py_heideltime']),
+      # trying to add files...
+      include_package_data=True,
+      package_data={
+            'Heideltime': ['*'],
+      },
       py_modules=['py_heideltime']
 )
 
