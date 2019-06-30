e89cb31ab157f8d3fb7fc26cf1553cb7bebb1460
JMendes1995 <mendesjorge49@gmail.com>
2019-06-30 12:40:37 +0100

    Update validate_input.py

--- py_heideltime/validate_input.py
+++ py_heideltime/validate_input.py
@@ -1,7 +1,7 @@
 def verify_temporal_tagger(language,date_granularity, document_type, document_creation_time):
     if language == '':
         print('You must select a valid language.\n')
-        return exit(1)
+        return {}
     elif date_granularity != 'full' and date_granularity != 'day'and date_granularity != 'month' and date_granularity != 'year' and date_granularity !='':
         print('You must select a valid date_granularity.\n'
               'options:\n'
@@ -9,7 +9,7 @@ def verify_temporal_tagger(language,date_granularity, document_type, document_cr
               '     year;\n'
               '     month:\n'
               '     day;')
-        return exit(1)
+        return {}
     elif document_type.lower() != 'news'and document_type.lower() != 'narrative' and document_type.lower() != 'colloquial' and document_type.lower() != 'scientific':
         print('You must select a valid document_type.\n'
               'options:\n'
@@ -17,4 +17,4 @@ def verify_temporal_tagger(language,date_granularity, document_type, document_cr
               '     narrative;\n'
               '     colloquial;\n'
               '     scientific;')
-        return exit(1)
+        return {}
