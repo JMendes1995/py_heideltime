85cdafc956ccd4ab0397b336f98c0fb7d994199e
JMendes1995 <35015528+JMendes1995@users.noreply.github.com>
2019-06-30 22:33:48 +0100

    Update validate_input.py

--- py_heideltime/validate_input.py
+++ py_heideltime/validate_input.py
@@ -1,7 +1,7 @@
 def verify_temporal_tagger(language,date_granularity, document_type, document_creation_time):
     from py_heideltime.lang import languages
     if language.lower() not in languages:
-        print('You must select a valid language.\n')
+        print('You must specify a valid language.\n')
         return {}
     elif date_granularity != 'full' and date_granularity != 'day'and date_granularity != 'month' and date_granularity != 'year':
         print('You must select a valid date_granularity.\n'
