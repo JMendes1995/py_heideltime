2225e406d34944db4439a2eef05f7b8be3abff4f
JMendes1995 <35015528+JMendes1995@users.noreply.github.com>
2019-07-08 15:58:04 +0100

    Update validate_input.py

--- py_heideltime/validate_input.py
+++ py_heideltime/validate_input.py
@@ -1,18 +1,18 @@
-def verify_temporal_tagger(language,date_granularity, document_type, document_creation_time):
+def verify_temporal_tagger(language,date_granularity, document_type):
     from py_heideltime.lang import languages
     if language.lower() not in languages:
-        print('You must specify a valid language.\n')
-        print('''Options:
-                      "English";
-                      "Portuguese";
-                      "Spanish";
-                      "Germany";
-                      "Dutch";
-                      "Italian";
-                      "French".''')
+        print('Please specify a valid language.\n')
+        print('Options:\n'
+              '      English;\n'
+              '      Portuguese;\n'
+              '      Spanish;\n'
+              '      Germany;\n'
+              '      Dutch;\n'
+              '      Italian;\n'
+              '      French.')
         return {}
     elif date_granularity != 'full' and date_granularity != 'day'and date_granularity != 'month' and date_granularity != 'year':
-        print('You must select a valid date_granularity.\n'
+        print('Please specify a valid date_granularity.\n'
               'options:\n'
               '     full;\n'
               '     year;\n'
@@ -20,7 +20,7 @@ def verify_temporal_tagger(language,date_granularity, document_type, document_cr
               '     day;')
         return {}
     elif document_type.lower() != 'news'and document_type.lower() != 'narrative' and document_type.lower() != 'colloquial' and document_type.lower() != 'scientific' and (document_type.lower() != '' or document_type.lower() == ''):
-        print('You must select a valid document_type.\n'
+        print('Please specify a valid document_type.\n'
               'options:\n'
               '     news;\n'
               '     narrative;\n'
