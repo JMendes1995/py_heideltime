de3e1b77bf55ed92230fe1ca57977ec7db09f85e
Hugo Sousa <hmosousa@gmail.com>
2023-01-10 18:13:21 +0000

    Fixed tests.

--- tests/test_py_heideltime.py
+++ tests/test_py_heideltime.py
@@ -11,24 +11,17 @@ def _test_json(
         **kwargs
 ):
     content = json.load(filepath.open())
-    timexs, normalized_text, time_ml = heideltime(text=content["text"], **kwargs)
-    timexs = list(map(list, timexs))
-
-    assert content["normalized_text"] in normalized_text
-    assert content["time_ml"] in time_ml
+    timexs = heideltime(text=content["text"], **kwargs)
     assert timexs == content["timexs"]
 
 
 def test_heideltime_emoji():
-    text = 'texto com emoji 😀'
-    expected_score = []
-    expected_time_ml = expected_normalized_text = 'texto com emoji '
-
-    score, normalized_text, time_ml = heideltime(text=text, language="Portuguese")
-
-    assert normalized_text == expected_normalized_text
-    assert time_ml == expected_time_ml
-    assert score == expected_score
+    text = 'texto com emoji 😀 escrito ontem.'
+    expected_timexs = [
+        {'text': 'ontem', 'tid': 't1', 'type': 'DATE', 'value': 'XXXX-XX-XX', 'span': [26, 31]}
+    ]
+    timexs = heideltime(text=text, language="Portuguese")
+    assert timexs == expected_timexs
 
 
 def test_heideltime_long_text():
@@ -36,56 +29,21 @@ def test_heideltime_long_text():
     _test_json(filepath, language="Portuguese")
 
 
-def test_heideltime_pt_wikipedia_planeta():
-    filepath = RESOURCES_PATH / "pt_wikipedia_planeta.json"
+def test_heideltime_pt_arquivo():
+    filepath = RESOURCES_PATH / "pt_arquivo.json"
     _test_json(filepath, language="Portuguese")
 
 
-def test_heideltime_pt_arquivo_jugular():
-    filepath = RESOURCES_PATH / "pt_arquivo_jugular.json"
+def test_heideltime_pt_wikipedia():
+    filepath = RESOURCES_PATH / "pt_wikipedia.json"
     _test_json(filepath, language="Portuguese")
 
 
-def test_heideltime_pt_wikipedia_25_de_abril():
-    filepath = RESOURCES_PATH / "pt_wikipedia_25_de_abril.json"
-    _test_json(filepath, language="Portuguese")
-
-
-def test_heideltime_pt_wikipedia_operacao_marques():
-    filepath = RESOURCES_PATH / "pt_wikipedia_operacao_marques.json"
-    _test_json(filepath, language="Portuguese")
-
-
-def test_heideltime_pt_dilma():
-    filepath = RESOURCES_PATH / "pt_dilma.json"
-    _test_json(filepath, language="Portuguese")
-
-
-def test_heideltime_en_boston_marathon():
-    filepath = RESOURCES_PATH / "en_boston_marathon.json"
-    _test_json(filepath, language="English")
-
-
-def test_heideltime_en_neuroscience():
-    filepath = RESOURCES_PATH / "en_neuroscience.json"
-    _test_json(filepath, language="English")
-
-
-def test_heideltime_en_haiti_earthquake():
-    filepath = RESOURCES_PATH / "en_haiti_earthquake.json"
-    _test_json(filepath, language="English")
-
-
-def test_heideltime_en_london():
-    filepath = RESOURCES_PATH / "en_london.json"
-    _test_json(filepath, language="English")
-
-
-def test_heideltime_en_london_options():
-    filepath = RESOURCES_PATH / "en_london_options.json"
+def test_heideltime_en_options():
+    filepath = RESOURCES_PATH / "en_options.json"
     _test_json(filepath, language="English", dct='1939-08-31')
 
 
-def test_heideltime_fr_helmut():
-    filepath = RESOURCES_PATH / "fr_helmut.json"
+def test_heideltime_fr():
+    filepath = RESOURCES_PATH / "fr.json"
     _test_json(filepath, language="French")
