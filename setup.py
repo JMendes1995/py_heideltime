b3230775fac79aab78ab4b83fa480cc886f11779
Hugo Sousa <hmosousa@gmail.com>
2023-01-11 06:48:38 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 import setuptools
+from pathlib import Path
+
+long_description = Path("README.md").read_text()
 
 setuptools.setup(
     name="py_heideltime",
-    version="2.0.0",
+    version="1.0.1",
     description="Python wrapper for HeidelTime temporal tagger.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Jorge Mendes, Ricardo Campos, and Hugo Sousa',
     author_email='hmosousa@gmail.com',
     url='https://github.com/hmosousa/py_heideltime.git',
